5bc9bd9525c11f4965a3412e4ed984ab652ad7df
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 21:46:10 -0700

    Create type_conversion.py

---
+++ type_conversion.py
@@ -0,0 +1,17 @@
+integer_value = int(input("Enter an integer value: "))
+float_value = float(input("Enter a decimal value: "))
+
+string_value = str(integer_value)
+boolean_value = bool(integer_value)
+
+print("\nInteger:", integer_value)
+print("Type:", type(integer_value))
+
+print("\nFloat:", float_value)
+print("Type:", type(float_value))
+
+print("\nString:", string_value)
+print("Type:", type(string_value))
+
+print("\nBoolean:", boolean_value)
+print("Type:", type(boolean_value))
